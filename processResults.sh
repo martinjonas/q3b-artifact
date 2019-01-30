c1a14651eaeb856f546131aade0cdb697f4c357e
Martin Jonáš <martin.jonas@mail.muni.cz>
2019-01-30 09:30:51 +0100

    Add running and processing scripts.

---
+++ processResults.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd results
+/home/xjonas/.local/bin/table-generator --no-diff -d -o . -n data -f csv ../experiments/results/boolector.*.results.boolector.xml.bz2 ../experiments/results/cvc4.*.results.cvc4.xml.bz2 ../experiments/results/q3b.*.results.q3b.xml.bz2  ../experiments/results/z3.*.results.z3.xml.bz2
+bash PROCESSRESULTS.sh
+Rscript PROCESSRESULTS.R
+pdflatex results.tex
+cd ..
