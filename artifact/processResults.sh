32fef660ff34d700a9f040a02c4dd40f3375e5c4
Martin Jonáš <martin.jonas@mail.muni.cz>
2019-02-08 08:47:27 +0100

    Fix path to table-generator.

--- artifact/processResults.sh
+++ artifact/processResults.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd results
-/home/xjonas/.local/bin/table-generator --no-diff -d -o . -n data -f csv ../experiments/results/boolector.*.results.boolector.xml.bz2 ../experiments/results/cvc4.*.results.cvc4.xml.bz2 ../experiments/results/q3b.*.results.q3b.xml.bz2  ../experiments/results/z3.*.results.z3.xml.bz2
+table-generator --no-diff -d -o . -n data -f csv ../experiments/results/boolector.*.results.boolector.xml.bz2 ../experiments/results/cvc4.*.results.cvc4.xml.bz2 ../experiments/results/q3b.*.results.q3b.xml.bz2  ../experiments/results/z3.*.results.z3.xml.bz2
 bash PROCESSRESULTS.sh
 Rscript PROCESSRESULTS.R
 pdflatex results.tex
